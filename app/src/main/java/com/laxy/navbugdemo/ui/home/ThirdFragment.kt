9be93d9addc67a7b997119630a5bed310cedd162
laxy <truelyyang@gmail.com>
2024-05-15 21:35:36 +0800

    init

---
+++ app/src/main/java/com/laxy/navbugdemo/ui/home/ThirdFragment.kt
@@ -0,0 +1,36 @@
+package com.laxy.navbugdemo.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.laxy.navbugdemo.databinding.FragmentSubHomeBinding
+import com.laxy.navbugdemo.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
