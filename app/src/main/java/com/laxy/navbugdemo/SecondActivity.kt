0e332e0f859559bde1bd18c677070f233feddfa6
laxy <truelyyang@gmail.com>
2024-05-15 23:25:38 +0800

    rename file

--- app/src/main/java/com/laxy/navbugdemo/SecondActivity.kt
+++ app/src/main/java/com/laxy/navbugdemo/SecondActivity.kt
@@ -1,14 +1,8 @@
 package com.laxy.navbugdemo
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.laxy.navbugdemo.databinding.ActivityMainBinding
 import com.laxy.navbugdemo.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
@@ -20,6 +14,6 @@ class SecondActivity : AppCompatActivity() {
         setContentView(binding.root)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navController = navHostFragment.navController
-        navController.setGraph(R.navigation.anoter_navigation)
+        navController.setGraph(R.navigation.zanoter_navigation)
     }
 }
